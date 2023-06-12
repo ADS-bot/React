3c6e1fbff2cf4b4c625c8c71df4459acd28f1e86
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-13 02:03:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, {useState} from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light'); //to check whether dark mode is on or not
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  } 
   return (
     <>
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode} toggleMode= {toggleMode} />
 <div className='container my-3'>
-<TextForm heading="Enter The Text"/>
+<TextForm heading="Enter The Text" mode={mode} />
+<About/>
 </div>
 
 
